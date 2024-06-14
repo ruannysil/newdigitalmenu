5d148fa5591333b503e09f6112c0217fa5f15985
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2024-06-13 22:10:13 -0300

    styled: add lib

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["excellentglobal.com.br", "covabra.vtexassets.com"],
+    domains: [
+      "excellentglobal.com.br",
+      "covabra.vtexassets.com",
+      "firebasestorage.googleapis.com",
+      "www.designi.com.br"
+    ],
   },
 };
 
